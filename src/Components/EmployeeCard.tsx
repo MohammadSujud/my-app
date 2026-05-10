2b0091241ab15808557269a21cc8f3870f06d9a7
Mohammad Sujud <Mohammadsujud1234@gmail.com>
2026-05-11 02:03:25 +0300

    Add employee card styling with gradient design

--- src/Components/EmployeeCard.tsx
+++ src/Components/EmployeeCard.tsx
@@ -1,3 +1,5 @@
+import styles from "./EmployeeCard.module.css";
+
 interface EmployeeCard {
   id: number;
   firstName: string;
@@ -13,15 +15,23 @@ interface EmployeeCard {
 
 function EmployeeCard(prop: EmployeeCard) {
   return (
-    <>
-      <div>
-        <h3>Department Of {prop.company.department}</h3>
-        <p>
-          Name: {prop.firstName} {prop.lastName}
-        </p>
-        <p>Email: {prop.email}</p>
+    <div className={styles.card}>
+      <div className={styles.department}>
+        {prop.company.department}
       </div>
-    </>
+      <div className={styles.name}>
+        {prop.firstName} {prop.lastName}
+      </div>
+      <div className={styles.info}>
+        <strong>Email:</strong> {prop.email}
+      </div>
+      <div className={styles.info}>
+        <strong>Company:</strong> {prop.company.name}
+      </div>
+      <div className={styles.info}>
+        <strong>Title:</strong> {prop.company.title}
+      </div>
+    </div>
   );
 }
 
